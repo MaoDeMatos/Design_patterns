b1100ed37bd8e1a0a007adbe66eaedaef61842d2
Mao De Matos <mao.dematos@gmail.com>
2021-10-26 11:49:02 +0200

    Added ListController

---
+++ DependencyInjection/Controllers/ListController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace DependencyInjection\Controllers;
+
+use DependencyInjection\Models\Todo\TodoItem;
+
+class ListController
+{
+  public $itemsList = [];
+
+  public function getItemsList()
+  {
+    return $this->itemsList;
+  }
+
+  public function addItem(TodoItem $item)
+  {
+    $this->itemsList[] = $item;
+  }
+}
--- DependencyInjection/index.php
+++ DependencyInjection/index.php
@@ -1,2 +1,10 @@
 <?php
 
+use DependencyInjection\Models\Todo\TodoItem;
+
+require $_SERVER['DOCUMENT_ROOT'] . '\autoload.php';
+
+$todoItem = new TodoItem();
+
+echo '<pre>';
+print_r($todoItem);
