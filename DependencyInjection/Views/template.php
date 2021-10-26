ead862ec09a96f9b46b1bcf42fd446ed5f5e50da
Mao De Matos <mao.dematos@gmail.com>
2021-10-26 11:19:04 +0200

    Added base style and vues

---
+++ DependencyInjection/Views/template.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" class="dark">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta http-equiv="Content-Type" content="text/html" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <!-- Stylesheets -->
+  <link rel="stylesheet" href="/DependencyInjection/public/css/style.min.css">
+  <link rel="stylesheet" href="/DependencyInjection/public/css/custom_rules.css">
+
+  <title>Dependency Injection</title>
+</head>
+
+<body class="color-theme-1">
+  <div id="sidebar" class="color-theme-2 flex f-center f-column">
+    <h2 class="highlighted">SideBar</h2>
+    <nav class="main-nav"></nav>
+  </div>
+
+  <div id="page" class="flex f-center">
+    <div><?= $content ?></div>
+  </div>
+</body>
+
+</html>
