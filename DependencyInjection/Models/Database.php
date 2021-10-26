f1cfd3dfcb22dc4eb25761063b1aea81951680b6
Mao De Matos <mao.dematos@gmail.com>
2021-10-26 11:03:31 +0200

    Updated dependency injection

---
+++ DependencyInjection/Models/Database.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Factory\Models;
+
+use DependencyInjection\Models\Interfaces\DatabaseI;
+
+abstract class Database implements DatabaseI
+{
+  protected static $db;
+  protected static $instance;
+
+  protected function __construct()
+  {
+  }
+
+  // Create a new singleton
+  protected static function createInstance($args)
+  {
+    // If class do not already exists
+    if (!isset(static::$instance)) {
+      // Create a new instance of this class
+      static::$instance = new static();
+      static::connect($args);
+    }
+  }
+
+  protected static function connect(array $settings)
+  {
+    // $db = new \PDO(
+    //   $settings['driver']
+    //     . ":host=" . $settings['host']
+    //     . ";dbname=" . $settings['dbname'],
+    //   $settings['user'],
+    //   $settings['pass']
+    // );
+    $db = 'mysql';
+    static::setDb($db);
+  }
+
+  public static function setDb($db)
+  {
+    static::$db = $db;
+  }
+
+  public static function getDb()
+  {
+    return static::$db;
+  }
+
+  public static function getInstance($args)
+  {
+    static::createInstance($args);
+    return static::$instance;
+  }
+}
---
+++ DependencyInjection/Models/Interfaces/DatabaseI.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace DependencyInjection\Models\Interfaces;
+
+interface DatabaseI {
+
+}
