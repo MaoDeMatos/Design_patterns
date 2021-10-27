6da49ca324e4f41a53f29ab193857920904b70c3
Mao De Matos <mao.dematos@gmail.com>
2021-10-27 12:20:27 +0200

    Strategy - Added base files

---
+++ Strategy/config.php
@@ -0,0 +1,4 @@
+<?php
+
+define('PROJECT_ROOT', MAIN_ROOT . 'Strategy/');
+define('PROJECT_HOST', MAIN_HOST . 'Strategy/');
---
+++ Strategy/index.php
@@ -0,0 +1,14 @@
+<?php
+
+require 'config.php';
+
+ob_start();
+
+// Output formatting
+// echo '<pre>';
+
+// Content here
+
+// echo '</pre>';
+
+$content = ob_get_clean();
