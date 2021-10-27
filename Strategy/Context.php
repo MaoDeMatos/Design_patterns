323644895fc9f46fead85f5c845acddb802e1c02
Clement431 <clembernard43@gmail.com>
2021-10-27 15:34:52 +0200

    modify context

--- Strategy/Context.php
+++ Strategy/Context.php
@@ -2,27 +2,27 @@
 
 namespace Strategy;
 
+
 class Context
 {
   
-    private Strategy $strategy;
+    private StrategyI $strategy;
 
-    public function __construct(Strategy $strategy)
+    public function __construct(StrategyI $strategy)
     {
         $this->strategy = $strategy;
     }
 
-    public function setStrategy(Strategy $strategy)
+    public function setStrategy(StrategyI $strategy)
     {
         $this->strategy = $strategy;
     }
 
     public function doSomeBusinessLogic(): void
     {
-        // ...
 
         echo "Context: Sorting data using the strategy \n";
-        $result = $this->strategy->doAlgorithm(["a", "b", "c", "d", "e"]);
+        $result = $this->strategy->execute(45,84);
         echo implode(",", $result) . "\n";
 
     }
