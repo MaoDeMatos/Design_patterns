26be7b6779c14b87f3abc72b324d016f1d777588
Mao De Matos <mao.dematos@gmail.com>
2021-10-28 09:54:35 +0200

    Command - Add base files

---
+++ Command/config.php
@@ -0,0 +1,4 @@
+<?php
+
+define('PROJECT_ROOT', MAIN_ROOT . 'Command/');
+define('PROJECT_HOST', MAIN_HOST . 'Command/');
---
+++ Command/index.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'config.php';
+
+ob_start();
+
+// Output formatting
+// echo '<pre>';
+
+// echo '</pre>';
+
+$content = ob_get_clean();
